0cbf410e0051d74b4b3f660c93bf296592e96519
Charlie-Doutch <cadoutch@student.christscollege.com>
2024-05-23 11:17:09 +1200

    23/05/24 Renamed a variable to make the code more readable

--- cube.py
+++ cube.py
@@ -4,9 +4,9 @@ class RubiksCube:
     
     # Rubiks Cube Class
 
-    def __init__(self, n = 3, colours = ['w', 'o', 'g', 'r', 'b', 'y'], state = None):
+    def __init__(self, dimensions = 3, colours = ['w', 'o', 'g', 'r', 'b', 'y'], state = None):
         """
-        n = number for width and height of cube
+        dimensions = number for width and height of cube
         colours = list with first letter of every colour
         state = String representing state of cube
 
@@ -14,27 +14,27 @@ class RubiksCube:
         """
 
         if state is None:
-            self.n = n
+            self.dimesions = dimensions
             self.colours = colours
             self.reset()
         else:
-            self.n = int((len(state) / 6) ** (0.5))
+            self.dimesions = int((len(state) / 6) ** (0.5))
             self.colours = []
             self.cube = [[[]]]
             for i, s in enumerate(state):
                 if s not in self.colours:
                     self.colours.append(s)
                     self.cube[-1][-1].append(s)
-                if len(self.cube[-1][-1]) == self.n and len(self.cube[-1]) < self.n:
+                if len(self.cube[-1][-1]) == self.dimesions and len(self.cube[-1]) < dimensions:
                     self.cube[-1].append([])
-                elif len(self.cube[-1][-1]) == self.n and len(self.cube[-1]) == self.n and i < len(state) -1:
+                elif len(self.cube[-1][-1]) == self.dimesions and len(self.cube[-1]) == self.dimesions and i < len(state) -1:
                     self.cube.append([[]])
 
     def reset(self):
         """
         Reset Cube
         """
-        self.cube = [[[c for x in range(self.n)] for y in range(self.n)] for c in self.colours]
+        self.cube = [[[c for x in range(self.dimesions)] for y in range(self.dimesions)] for c in self.colours]
 
     def solved(self):
         """
