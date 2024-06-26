1bde5774aadbfbf80bd5182aed92b013b1e9f1be
Charlie-Doutch <cadoutch@student.christscollege.com>
2024-06-27 11:26:17 +1200

    27/06/24 - Made cubes start on colour of side on, done from macbook in class

--- main_window.py
+++ main_window.py
@@ -1,7 +1,6 @@
 import sys
-
 import pygame
-    
+
 pygame.init()
 
 pygame.display.set_caption('RCS')
@@ -11,9 +10,9 @@ screen = pygame.display.set_mode((1024, 768))
 clock = pygame.time.Clock()
 
 class Block:
-    def __init__(self, x, y, width, height, block_colours):
+    def __init__(self, x, y, width, height, initial_colour, block_colours):
         self.rect = pygame.Rect(x, y, width, height)
-        self.clicked = 0
+        self.clicked = block_colours.index(initial_colour)
         self.block_colours = block_colours
 
     def change_colour(self, event):
@@ -27,60 +26,60 @@ class Blocks:
     def __init__(self):
         self.block_colours = ['White', 'Green', 'Red', 'Blue', 'Orange', 'Yellow']
         self.blocks = [
-            #White Side
-            Block(347, 50, 50, 50, self.block_colours),
-            Block(402, 50, 50, 50, self.block_colours),
-            Block(457, 50, 50, 50, self.block_colours),
-            Block(347, 105, 50, 50, self.block_colours),
-            Block(457, 105, 50, 50, self.block_colours),
-            Block(347, 160, 50, 50, self.block_colours),
-            Block(402, 160, 50, 50, self.block_colours),
-            Block(457, 160, 50, 50, self.block_colours),
-            #Red Side
-            Block(347, 220, 50, 50, self.block_colours),
-            Block(402, 220, 50, 50, self.block_colours),
-            Block(457, 220, 50, 50, self.block_colours),
-            Block(347, 275, 50, 50, self.block_colours),
-            Block(457, 275, 50, 50, self.block_colours),
-            Block(347, 330, 50, 50, self.block_colours),
-            Block(402, 330, 50, 50, self.block_colours),
-            Block(457, 330, 50, 50, self.block_colours),
-            #Green Side
-            Block(177, 220, 50, 50, self.block_colours),
-            Block(232, 220, 50, 50, self.block_colours),
-            Block(287, 220, 50, 50, self.block_colours),
-            Block(177, 275, 50, 50, self.block_colours),
-            Block(287, 275, 50, 50, self.block_colours),
-            Block(177, 330, 50, 50, self.block_colours),
-            Block(232, 330, 50, 50, self.block_colours),
-            Block(287, 330, 50, 50, self.block_colours),
-            #Blue Side
-            Block(517, 220, 50, 50, self.block_colours),
-            Block(572, 220, 50, 50, self.block_colours),
-            Block(627, 220, 50, 50, self.block_colours),
-            Block(517, 275, 50, 50, self.block_colours),
-            Block(627, 275, 50, 50, self.block_colours),
-            Block(517, 330, 50, 50, self.block_colours),
-            Block(572, 330, 50, 50, self.block_colours),
-            Block(627, 330, 50, 50, self.block_colours),
-            #Orange Side
-            Block(687, 220, 50, 50, self.block_colours),
-            Block(742, 220, 50, 50, self.block_colours),
-            Block(797, 220, 50, 50, self.block_colours),
-            Block(687, 275, 50, 50, self.block_colours),
-            Block(797, 275, 50, 50, self.block_colours),
-            Block(687, 330, 50, 50, self.block_colours),
-            Block(742, 330, 50, 50, self.block_colours),
-            Block(797, 330, 50, 50, self.block_colours),
-            #Yellow Side
-            Block(347, 390, 50, 50, self.block_colours),
-            Block(402, 390, 50, 50, self.block_colours),
-            Block(457, 390, 50, 50, self.block_colours),
-            Block(347, 445, 50, 50, self.block_colours),
-            Block(457, 445, 50, 50, self.block_colours),
-            Block(347, 500, 50, 50, self.block_colours),
-            Block(402, 500, 50, 50, self.block_colours),
-            Block(457, 500, 50, 50, self.block_colours),
+            # White Side
+            Block(347, 50, 50, 50, 'White', self.block_colours),
+            Block(402, 50, 50, 50, 'White', self.block_colours),
+            Block(457, 50, 50, 50, 'White', self.block_colours),
+            Block(347, 105, 50, 50, 'White', self.block_colours),
+            Block(457, 105, 50, 50, 'White', self.block_colours),
+            Block(347, 160, 50, 50, 'White', self.block_colours),
+            Block(402, 160, 50, 50, 'White', self.block_colours),
+            Block(457, 160, 50, 50, 'White', self.block_colours),
+            # Red Side
+            Block(347, 220, 50, 50, 'Red', self.block_colours),
+            Block(402, 220, 50, 50, 'Red', self.block_colours),
+            Block(457, 220, 50, 50, 'Red', self.block_colours),
+            Block(347, 275, 50, 50, 'Red', self.block_colours),
+            Block(457, 275, 50, 50, 'Red', self.block_colours),
+            Block(347, 330, 50, 50, 'Red', self.block_colours),
+            Block(402, 330, 50, 50, 'Red', self.block_colours),
+            Block(457, 330, 50, 50, 'Red', self.block_colours),
+            # Green Side
+            Block(177, 220, 50, 50, 'Green', self.block_colours),
+            Block(232, 220, 50, 50, 'Green', self.block_colours),
+            Block(287, 220, 50, 50, 'Green', self.block_colours),
+            Block(177, 275, 50, 50, 'Green', self.block_colours),
+            Block(287, 275, 50, 50, 'Green', self.block_colours),
+            Block(177, 330, 50, 50, 'Green', self.block_colours),
+            Block(232, 330, 50, 50, 'Green', self.block_colours),
+            Block(287, 330, 50, 50, 'Green', self.block_colours),
+            # Blue Side
+            Block(517, 220, 50, 50, 'Orange', self.block_colours),
+            Block(572, 220, 50, 50, 'Orange', self.block_colours),
+            Block(627, 220, 50, 50, 'Orange', self.block_colours),
+            Block(517, 275, 50, 50, 'Orange', self.block_colours),
+            Block(627, 275, 50, 50, 'Orange', self.block_colours),
+            Block(517, 330, 50, 50, 'Orange', self.block_colours),
+            Block(572, 330, 50, 50, 'Orange', self.block_colours),
+            Block(627, 330, 50, 50, 'Orange', self.block_colours),
+            # Orange Side
+            Block(687, 220, 50, 50, 'Blue', self.block_colours),
+            Block(742, 220, 50, 50, 'Blue', self.block_colours),
+            Block(797, 220, 50, 50, 'Blue', self.block_colours),
+            Block(687, 275, 50, 50, 'Blue', self.block_colours),
+            Block(797, 275, 50, 50, 'Blue', self.block_colours),
+            Block(687, 330, 50, 50, 'Blue', self.block_colours),
+            Block(742, 330, 50, 50, 'Blue', self.block_colours),
+            Block(797, 330, 50, 50, 'Blue', self.block_colours),
+            # Yellow Side
+            Block(347, 390, 50, 50, 'Yellow', self.block_colours),
+            Block(402, 390, 50, 50, 'Yellow', self.block_colours),
+            Block(457, 390, 50, 50, 'Yellow', self.block_colours),
+            Block(347, 445, 50, 50, 'Yellow', self.block_colours),
+            Block(457, 445, 50, 50, 'Yellow', self.block_colours),
+            Block(347, 500, 50, 50, 'Yellow', self.block_colours),
+            Block(402, 500, 50, 50, 'Yellow', self.block_colours),
+            Block(457, 500, 50, 50, 'Yellow', self.block_colours),
         ]
 
     def change_colour(self, event):
@@ -94,10 +93,8 @@ class Blocks:
 blocks = Blocks()
 
 cube_net = pygame.image.load("Rubiks-Cube-Solver/Assets/Cube_Net.png").convert_alpha()
- 
 
 while True:
-
     screen.fill((255, 184, 179))
 
     screen.blit(cube_net, (0, 0))
@@ -113,6 +110,6 @@ while True:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:  # Left mouse button
                 blocks.change_colour(event)
-    
+
     pygame.display.update()
     clock.tick(60)
