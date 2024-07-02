744662dd98436af513663dfca85f80aa7059d1a2
Charlie-Doutch <cadoutch@student.christscollege.com>
2024-07-02 15:36:45 +1200

    02/06/24 - Created function for creating string based off user inputed cube state.

--- main_window.py
+++ main_window.py
@@ -9,6 +9,16 @@ screen = pygame.display.set_mode((1024, 768))
 
 clock = pygame.time.Clock()
 
+# Define color to letter mapping
+colour_to_letter = {
+    'White': 'w',
+    'Green': 'g',
+    'Red': 'r',
+    'Blue': 'b',
+    'Orange': 'o',
+    'Yellow': 'y'
+}
+
 class Block:
     def __init__(self, x, y, width, height, initial_colour, block_colours):
         self.rect = pygame.Rect(x, y, width, height)
@@ -21,6 +31,9 @@ class Block:
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.block_colours[self.clicked], self.rect)
+
+    def get_colour(self):
+        return self.block_colours[self.clicked]
 
 class Blocks:
     def __init__(self):
@@ -35,15 +48,6 @@ class Blocks:
             Block(347, 160, 50, 50, 'White', self.block_colours),
             Block(402, 160, 50, 50, 'White', self.block_colours),
             Block(457, 160, 50, 50, 'White', self.block_colours),
-            # Red Side
-            Block(347, 220, 50, 50, 'Red', self.block_colours),
-            Block(402, 220, 50, 50, 'Red', self.block_colours),
-            Block(457, 220, 50, 50, 'Red', self.block_colours),
-            Block(347, 275, 50, 50, 'Red', self.block_colours),
-            Block(457, 275, 50, 50, 'Red', self.block_colours),
-            Block(347, 330, 50, 50, 'Red', self.block_colours),
-            Block(402, 330, 50, 50, 'Red', self.block_colours),
-            Block(457, 330, 50, 50, 'Red', self.block_colours),
             # Green Side
             Block(177, 220, 50, 50, 'Green', self.block_colours),
             Block(232, 220, 50, 50, 'Green', self.block_colours),
@@ -53,6 +57,15 @@ class Blocks:
             Block(177, 330, 50, 50, 'Green', self.block_colours),
             Block(232, 330, 50, 50, 'Green', self.block_colours),
             Block(287, 330, 50, 50, 'Green', self.block_colours),
+            # Red Side
+            Block(347, 220, 50, 50, 'Red', self.block_colours),
+            Block(402, 220, 50, 50, 'Red', self.block_colours),
+            Block(457, 220, 50, 50, 'Red', self.block_colours),
+            Block(347, 275, 50, 50, 'Red', self.block_colours),
+            Block(457, 275, 50, 50, 'Red', self.block_colours),
+            Block(347, 330, 50, 50, 'Red', self.block_colours),
+            Block(402, 330, 50, 50, 'Red', self.block_colours),
+            Block(457, 330, 50, 50, 'Red', self.block_colours),
             # Blue Side
             Block(517, 220, 50, 50, 'Blue', self.block_colours),
             Block(572, 220, 50, 50, 'Blue', self.block_colours),
@@ -81,6 +94,14 @@ class Blocks:
             Block(402, 500, 50, 50, 'Yellow', self.block_colours),
             Block(457, 500, 50, 50, 'Yellow', self.block_colours),
         ]
+        self.centres = {
+            'White': 'w',
+            'Green': 'g',
+            'Red': 'r',
+            'Blue': 'b',
+            'Orange': 'o',
+            'Yellow': 'y'
+        }
 
     def change_colour(self, event):
         for block in self.blocks:
@@ -89,6 +110,21 @@ class Blocks:
     def draw(self, screen):
         for block in self.blocks:
             block.draw(screen)
+
+    def get_colours_string(self):
+        # Insert center pieces at the correct position within each face string
+        def insert_center(colour_string, centre):
+            return colour_string[:4] + centre + colour_string[4:]
+
+        colors_string = [
+            insert_center(''.join(colour_to_letter[block.get_colour()] for block in self.blocks[0:8]), self.centres['White']),
+            insert_center(''.join(colour_to_letter[block.get_colour()] for block in self.blocks[8:16]), self.centres['Green']),
+            insert_center(''.join(colour_to_letter[block.get_colour()] for block in self.blocks[16:24]), self.centres['Red']),
+            insert_center(''.join(colour_to_letter[block.get_colour()] for block in self.blocks[24:32]), self.centres['Blue']),
+            insert_center(''.join(colour_to_letter[block.get_colour()] for block in self.blocks[32:40]), self.centres['Orange']),
+            insert_center(''.join(colour_to_letter[block.get_colour()] for block in self.blocks[40:48]), self.centres['Yellow']),
+        ]
+        return ''.join(colors_string)
 
 blocks = Blocks()
 
@@ -101,14 +137,16 @@ while True:
 
     blocks.draw(screen)
 
-    mouse_pos = pygame.mouse.get_pos
+    # Get and print the color string
+    colours_string = blocks.get_colours_string()
+    print(colours_string)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:  # Left mouse button
+            if event.button == 1:
                 blocks.change_colour(event)
 
     pygame.display.update()
